922778b98258dde9f66afb11e5d7a3b7280e9d16
melguz78 <melvin.guzman20@itca.edu.sv>
2021-04-13 14:56:48 -0600

    secretaria.php

---
+++ secretaria.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+$nom= $_SESSION['txtUsuario'];
+echo "<h4>Bienvenido:$nom</h4>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+    
+            <ul>
+                <li><a href="#">Actividades</a></li>
+                <li><a href="#">Enlaces</a></li>
+                <li><a href="#">intranet</a></li>
+            
+            </ul>
+    
+    
+    </nav>
+</body>
+</html>
